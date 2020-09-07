013acba13d6b156831232cb28bf6f5f64ef3cc62
GitHub Action <action@github.com>
2020-09-07 13:53:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/janrankenhohn/eyexprestudy/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.janrankenhohn.eyexprestudy.services
+
+import com.github.janrankenhohn.eyexprestudy.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/janrankenhohn/eyexprestudy/services/MyProjectService.kt
+++ src/main/kotlin/com/github/janrankenhohn/eyexprestudy/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.janrankenhohn.eyexprestudy.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.janrankenhohn.eyexprestudy.MyBundle
 
 class MyProjectService(project: Project) {
 
